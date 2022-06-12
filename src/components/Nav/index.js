2802e1f5fd8f5f3df238a1adc5842d3e97d27db7
sacylkowski <sacylkowski@gmail.com>
2022-06-11 23:19:19 -0500

    added about section

---
+++ src/components/Nav/index.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+const categories = [
+    {
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+  ];
+
+  function categorySelected() {
+    console.log(`clicked`)
+  }
+
+function Nav() {
+    return (
+    <header>
+        <h2>
+        <a href="/">
+            <span role="img" aria-label="camera">📸</span> Oh snap!
+        </a>
+        </h2>
+        <nav>
+            <ul className="flex-row">
+                <li className="mx-2">
+                    <a href="#about">
+                        About me
+                    </a>
+                </li>
+                <li>
+                    <span>Contact</span>
+                </li>
+                {/* whenever you map over anything in JSX, the outermost element must have a key attribute */}
+                {categories.map((category) => (
+                    <li
+                    className="mx-1"
+                    key={category.name}
+                    >
+                        <span onClick={() => categorySelected(category.name)} >
+                            {category.name}
+                        </span>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    </header>
+    );
+  }
+
+
+  export default Nav;

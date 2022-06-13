cc9faf54fde1f1bf15276a0dc5f4289bf2bbfa78
sacylkowski <sacylkowski@gmail.com>
2022-06-12 22:06:51 -0500

    added Nav tests

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -22,14 +22,15 @@ function Nav() {
     return (
     <header>
         <h2>
-        <a href="/">
-            <span role="img" aria-label="camera">📸</span> Oh snap!
+            {/* the value of the data-testid attributes are link for the homepage and about for the about page */}
+        <a data-testid="link" href="/">
+            <span role="img" aria-label="camera"> 📸</span> Oh Snap!
         </a>
         </h2>
         <nav>
             <ul className="flex-row">
                 <li className="mx-2">
-                    <a href="#about">
+                    <a data-testid="about" href="#about">
                         About me
                     </a>
                 </li>
